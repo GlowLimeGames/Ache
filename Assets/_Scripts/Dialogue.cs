1a850cb9ab357b3761427530f83f99ee4202216a
Max Silbiger <hollowaytape@retro-type.com>
2018-01-09 11:30:06 -0500

    Zooming

--- Assets/_Scripts/Dialogue.cs
+++ Assets/_Scripts/Dialogue.cs
@@ -5,13 +5,15 @@ using UnityEngine.UI;
 
 public class Dialogue : MonoBehaviour {
 	public Canvas canv;
+	private Camera cam;
 	public Transform bubble;   // the prefab
 
 	public bool sceneRunning;
 	private Transform activeBubble;
 	private int bubbleIndex;
 
-	[SerializeField] private float fadePerSecond = 2.5f;
+	private float zoomPerSecond = 2.5f;
+	private float fadePerSecond = 2.5f;
 	private bool fadingIn, fadingOut;
 	private Color white = new Color(1.0f, 1.0f, 1.0f, 1.0f);
 	private Color transparent = new Color (1.0f, 1.0f, 1.0f, 0.0f);
@@ -20,10 +22,17 @@ public class Dialogue : MonoBehaviour {
 
 	string[] lines = new string[] { "First text", "Second text", "Third text", "Fourth text", };
 
+	private bool zoomingIn, zoomingOut;
+
+	private float defaultZoom = 5.0f;
+	private float sceneZoom = 4.0f;
+
 	// Use this for initialization
 	void Start() {
+		cam = Camera.main;
 		sceneRunning = false;
 		fadingIn = fadingOut = false;
+		zoomingIn = zoomingOut = false;
 		bubbleIndex = 0;
 	}
 
@@ -31,20 +40,47 @@ public class Dialogue : MonoBehaviour {
 		bubbleIndex = index;
 		sceneRunning = true;
 		DisplayText (bubbleIndex, offset);
+
+		//cam.orthographicSize = sceneZoom;
+		ZoomIn();
 	}
 
 	public void EndScene() {
 		sceneRunning = false;
+
+		//cam.orthographicSize = defaultZoom;
+		ZoomOut();
+	}
+
+	public void ZoomIn() {
+		zoomingIn = true;
+	}
+
+	public void ZoomOut() {
+		zoomingOut = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (zoomingIn) {
+			cam.orthographicSize -= (zoomPerSecond * Time.deltaTime);
+			if (cam.orthographicSize < sceneZoom) {
+				zoomingIn = false;
+			}
+		}
+
+		if (zoomingOut) {
+			cam.orthographicSize += (zoomPerSecond * Time.deltaTime);
+			if (cam.orthographicSize > defaultZoom) {
+				zoomingOut = false;
+			}
+		}
 		// Check for input, advance text if left click
 
 		if (sceneRunning) {
 			if ((!fadingIn) && (!fadingOut) && (Input.GetMouseButtonDown (0))) {
 				RemoveText ();
-
 			}
 
 			if (fadingIn) {
@@ -86,12 +122,12 @@ public class Dialogue : MonoBehaviour {
 	}
 
 	IEnumerator StopFadeIn() {
-		yield return new WaitForSeconds (0.7f);
+		yield return new WaitForSeconds (0.4f);
 		fadingIn = false;
 	}
 		
 	IEnumerator NextWindow() {
-		yield return new WaitForSeconds (0.7f);
+		yield return new WaitForSeconds (0.4f);
 		activeBubble.gameObject.SetActive (false);
 		fadingOut = false;
 
