69017a5d01849c5aced9c0fe5ce7056fb628de5c
nault22e <32230140+nault22e@users.noreply.github.com>
2018-01-11 13:50:47 -0500

    Add files via upload

---
+++ Assets/Scripts/RopePuzzle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopePuzzle : MonoBehaviour {
+
+	public GameObject metalShard;
+	public GameObject rope;
+	public GameObject sword;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision coll){
+		if (coll.gameObject.tag == "MetalShard") {
+			//Not sure if the following line is necessary
+			metalShard = coll.gameObject;
+			//remove metal shard from inventory
+			print ("rope attained, now they are combined" +
+				"to make the sword");
+			//Automatically combine to make sword
+		}
+	}
+}
