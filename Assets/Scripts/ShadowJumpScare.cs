90fdfdf22b7b286e200fa494b8183a4e45a79928
Emilyann Nault <emnault15@gmail.com>
2018-01-12 11:34:49 -0500

    This should be all the things

---
+++ Assets/Scripts/ShadowJumpScare.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowJumpScare : MonoBehaviour {
+
+	public GameObject player;
+	public Sprite shadow;
+	//static Random random = new Random();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision coll){
+		if (coll.gameObject.tag == "Player") {
+			//Have shadow pop up x distance away from player
+			//at random time from when trigger has been collided with
+			//player.GetComponenet(). 
+			//yield return new WaitForSeconds(GetRandomNumber(0.0, 6.0));
+			Vector3 position = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0);
+			Instantiate(shadow, position, Quaternion.identity);
+
+		}
+	}
+
+	void OnTriggerEnter(Collider coll){
+		StartCoroutine (ShadowJumpScare_CR ());
+	}
+
+	IEnumerator ShadowJumpScare_CR(){
+		shadow.SetActive(true);
+	}
+
+	double GetRandomNumber(double minimum, double maximum)
+	{ 
+		return Random.value * (maximum - minimum) + minimum;
+	}
+}
