b6c4a44993e1b5f0ffff057e87d42adaa5d966e9
mcasan <31909324+mcasan@users.noreply.github.com>
2018-01-16 11:40:38 -0500

    add it

---
+++ Assets/_Scripts/StartScreen(Miranda)/StartScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class StartScreen : MonoBehaviour {
+
+	public void Start(){
+	
+	Debug.Log("StartGame");
+	
+	}
+
+	public void Credits(){
+
+	Debug.Log ("Credits");
+
+
+
+
+	}
+		
+	public void Exit(){
+	
+
+	Debug.Log ("Exit");
+	Application.Quit ();
+
+	}
+
+
+}
