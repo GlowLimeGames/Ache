0f4065b4529b342a2692cd36ab256eac7f9ad21f
Emilyann Nault <emnault15@gmail.com>
2018-01-19 14:52:45 -0500

    Some additions to boulder scene

---
+++ Assets/Puzzles/Scripts/AttainHandle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttainHandle : MonoBehaviour {
+
+	void OnMouseDown(){
+		Inventory.Instance.AddItem (1);
+		//Switch to crushed tree sprite and add stick if
+		//boulder has collided with tree object
+		print("handle attained");
+	}
+}
